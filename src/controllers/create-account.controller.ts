ce8ddc3ecec62f35838fc1a7f6ce1488c21818d0
Henry <franzhenry46@gmail.com>
2024-07-27 01:14:56 -0300

    create controller account

---
+++ src/controllers/create-account.controller.ts
@@ -0,0 +1,52 @@
+import { Body, Controller, HttpCode, Post, UsePipes } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { hash } from 'bcrypt';
+import { ConflictException } from '@nestjs/common';
+import { z } from 'zod';
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe';
+
+const createAccountBodySchema = z.object({
+  name: z.string().min(3),
+  email: z.string().email(),
+  password: z.string().min(6),
+});
+
+type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>;
+
+@Controller('create-account')
+export class CreateAccountController {
+  constructor(private prisma: PrismaService) {}
+
+  @Post()
+  @HttpCode(201)
+  @UsePipes(new ZodValidationPipe(createAccountBodySchema))
+  async handle(@Body() body: CreateAccountBodySchema) {
+    const { name, email, password } = body;
+
+    const passwordHash = await hash(password, 8);
+
+    const userWithSameEmail = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (userWithSameEmail) {
+      throw new ConflictException(
+        'User with same e-mail address already exists.',
+      );
+    }
+
+    await this.prisma.user.create({
+      data: {
+        email,
+        name,
+        password_hash: passwordHash,
+      },
+    });
+
+    return {
+      message: 'user created successfully',
+    };
+  }
+}
