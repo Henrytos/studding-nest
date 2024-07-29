47be16bba3554cab553ab0fbd104fdfe342dd006
Henry <franzhenry46@gmail.com>
2024-07-29 15:45:59 -0300

    utilizando config module no nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
 import { CreateAccountController } from './controllers/create-account.controller';
+import { ConfigModule } from '@nestjs/config';
+import { envSchema } from './env';
 
 //orquestrador de dependecias
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      validate: (obj) => envSchema.parse(obj),
+    }),
+  ],
   controllers: [CreateAccountController],
   providers: [PrismaService],
 })
