b707f632afbc63d5b2832c87739424fb3f8d3ee4
HENRY <113705213+Henrytos@users.noreply.github.com>
2024-08-03 15:57:07 -0300

    criando testes e2e

---
+++ src/controllers/authenticate.controller.e2e-spec.ts
@@ -0,0 +1,46 @@
+import { AppModule } from '@/app.module';
+import { PrismaService } from '@/prisma/prisma.service';
+import { INestApplication } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Test } from '@nestjs/testing';
+import { hash } from 'bcrypt';
+import request from 'supertest';
+
+describe('Authenticate (E2E)', () => {
+  let app: INestApplication;
+  let prisma: PrismaService;
+  let jwt: JwtService;
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleRef.createNestApplication();
+
+    prisma = moduleRef.get(PrismaService);
+
+    jwt = moduleRef.get(JwtService);
+
+    await app.init();
+  });
+
+  test('[POST] /sessions', async () => {
+    await prisma.user.create({
+      data: {
+        name: 'johndoe',
+        email: 'johndoe@example.com',
+        password_hash: await hash('123456', 8),
+      },
+    });
+    const response = await request(app.getHttpServer()).post('/sessions').send({
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual({
+      access_token: expect.any(String),
+    });
+  });
+});
